3867de7165a9b147ce9f85eebd4d0050769428a0
Ridoy-Mojumder <hridoymoju463@gmail.com>
2024-04-27 03:42:37 +0600

    add update server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -24,7 +24,69 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const artAndCraftCollection = client.db('artAndCraftDb').collection('artAndCraft');
+
+    app.get('/addCraft', async (req, res) => {
+      const artAndCraft = artAndCraftCollection.find();
+      const result = await artAndCraft.toArray();
+      res.send(result)
+    })
+
+    
+
+
+    app.get('/addCraft/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await artAndCraftCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.post('/addCraft', async (req, res) => {
+      const newArtAndCraft = req.body;
+      console.log(newArtAndCraft);
+      const result = await artAndCraftCollection.insertOne(newArtAndCraft);
+      res.send(result);
+    })
+
+    app.put('/addCraft/:id', async(req,res)=>{
+      const id = req.params.id;
+      const user = req.body;
+      console.log(user)
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true }
+      const updateUser = {
+        $set : {
+          photoUrl: user.photoUrl,
+          itemName: user.itemName,
+          subcategoryName: user.subcategoryName,
+          shortDescription: user.shortDescription,
+          price: user.price,
+          rating: user.rating,
+          customization: user.customization,
+          processingTime: user.processingTime,
+          stockStatus: user.stockStatus,
+
+        }
+      }
+      const result = await artAndCraftCollection.updateOne(filter, updateUser, options)
+      res.send(result)
+    })
+
+
+    app.delete('/addCraft/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log('delete id:', id);
+      const query = { _id: new ObjectId(id) };
+      const result = await artAndCraftCollection.deleteOne(query);
+      res.send(result)
+    })
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -41,9 +103,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("Assignment ten server is running")
-  })
-  
-  app.listen(port, () => {
-    console.log(`Assignment ten server is running on port: ${port}`)
-  })
+  res.send("Assignment ten server is running")
+})
+
+app.listen(port, () => {
+  console.log(`Assignment ten server is running on port: ${port}`)
+})
